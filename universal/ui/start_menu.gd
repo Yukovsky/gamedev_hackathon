d1a227b187f26421618fe955ca8b24279d501446
OrDinaD <Vlad.vasilevskiy.07@gmail.com>
2026-03-28 15:56:00 +0300

    fix(ui): aggressive debug menu with huge buttons and process-based input

--- universal/ui/start_menu.gd
+++ universal/ui/start_menu.gd
@@ -1,25 +1,21 @@
 extends Control
 
 @onready var btn_start: Button = %BtnStart
-@onready var btn_quit: Button = %BtnQuit
 
 func _ready() -> void:
-	print("--- МЕНЮ ГОТОВО ---")
+	print("--- МЕНЮ ЗАПУЩЕНО ---")
+	# Убедимся, что кнопки видны и активны
+	btn_start.disabled = false
+	btn_start.mouse_filter = Control.MOUSE_FILTER_STOP
 	
+	# Прямая привязка сигнала
 	btn_start.pressed.connect(_on_btn_start_pressed)
-	btn_quit.pressed.connect(_on_btn_quit_pressed)
-	
-	# Проверка наведения для отладки
-	btn_start.mouse_entered.connect(func(): print("МЫШЬ НА КНОПКЕ 'ИГРАТЬ'"))
-	btn_quit.mouse_entered.connect(func(): print("МЫШЬ НА КНОПКЕ 'ВЫХОД'"))
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		print("КЛИК В МЕНЮ (ПОЗИЦИЯ: ", event.position, ")")
+func _process(_delta: float) -> void:
+	# Если клик прошел мимо кнопки, но в окно - мы это увидим
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		print("Обнаружен зажатый клик (Mouse Left) в кадре")
 
 func _on_btn_start_pressed() -> void:
-	print("--- ЗАПУСК ИГРЫ ---")
+	print("!!! КНОПКА ИГРАТЬ СРАБОТАЛА !!!")
 	get_tree().change_scene_to_file("res://main.tscn")
-
-func _on_btn_quit_pressed() -> void:
-	get_tree().quit()
