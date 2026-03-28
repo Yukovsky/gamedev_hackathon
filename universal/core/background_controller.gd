850452f37c56b2d942e2f5e709e5aff661aec435
OrDinaD <Vlad.vasilevskiy.07@gmail.com>
2026-03-28 18:31:06 +0300

    Fix: Remove corrupted sprites, revert to static background

--- universal/core/background_controller.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-@export var scroll_speed: float = 100.0
-
-func _process(delta: float) -> void:
-	if get_parent() is ParallaxBackground:
-		get_parent().scroll_offset.y += scroll_speed * delta
